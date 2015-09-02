0227174f6f579e292b7f7e1d36c25108180b78a6
Edwin Wiersma <eddie1707@live.nl>
2015-09-02 14:36:27 +0200

    first commit

---
+++ SprintEnglish/FoodDrink2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FoodDrink2ViewController.swift
+//  SprintEnglish
+//
+//  Created by Edwin Wiersma on 02/06/15.
+//  Copyright (c) 2015 Apps4Mobile. All rights reserved.
+//
+
+import UIKit
+
+class FoodDrink2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
